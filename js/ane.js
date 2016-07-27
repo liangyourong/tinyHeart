23f7dbe4a7b5ea9e35c35aa8f3bcd4af1011a8d3
you <18710890843@163.com>
2016-07-27 17:57:49 +0800

    1
    1

---
+++ js/ane.js
@@ -0,0 +1,41 @@
+var aneObj = function(){
+
+	//start point,control point,end point(sin)
+	this.rootx = [];
+	this.headx = [];
+	this.heady = [];
+	this.alpha=0;
+	this.amp=[];
+
+}
+
+aneObj.prototype.num=50;
+aneObj.prototype.init=function(){
+	for(var i=0;i<this.num;i++){
+		this.rootx[i]=i*16+Math.random()*20;//[0,1)
+		this.headx[i]=this.rootx[i];
+		this.heady[i]= canHeight - 220 +Math.random()*50;
+		this.amp[i]=Math.random()*50+50;
+	}
+};
+aneObj.prototype.draw=function(){
+	this.alpha+=deltaTime*0.0006;
+	var l =Math.sin(this.alpha); //[-1,1]
+
+	ctx2.save();
+	ctx2.globalAlpha=0.6;
+	ctx2.lineWidth=20;
+	ctx2.lineCap="round";
+	ctx2.strokeStyle="#3b154e";
+	for(var i=0;i<this.num;i++)
+	{
+		ctx2.beginPath();
+		ctx2.moveTo(this.rootx[i],canHeight);
+		this.headx[i]=this.rootx[i]+l*this.amp[i]
+		ctx2.quadraticCurveTo(this.rootx[i],canHeight-80, this.headx[i],this.heady[i]);
+		
+		ctx2.stroke();
+
+	}
+	ctx2.restore();
+};
